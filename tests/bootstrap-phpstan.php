fc371ec7573e448be847b71d3465876108d63b55
yrbane <yrbane@nethttp.net>
2026-05-05 17:14:18 +0200

    chore: configure PHPStan niveau 8 avec stubs WordPress

---
+++ tests/bootstrap-phpstan.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Bootstrap PHPStan : pas d'init nécessaire pour le moment.
+ * Existe pour être référencé depuis phpstan.neon (extensible).
+ *
+ * @package OliTheme\Tests
+ */
