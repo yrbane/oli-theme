b56a8bfc429c8862e5ff5744db8734b106a97f33
yrbane <yrbane@nethttp.net>
2026-05-16 13:07:31 +0200

    Maj théme

--- .cache/templates/0bd06065a764a9a47b08c3f0ccebaacf.php
+++ .cache/templates/0bd06065a764a9a47b08c3f0ccebaacf.php
@@ -6,27 +6,26 @@
     <meta charset="<?= htmlspecialchars((string)($charset ?? ''), ENT_QUOTES, 'UTF-8') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php if (!empty($seo)): ?>
-        [% include 'partials/seo-head.html.tpl' %]
+        <?= $this->renderInclude('partials/seo-head.html.tpl', array_merge(get_defined_vars(), [])) ?>
     <?php else: ?>
         <title><?= htmlspecialchars((string)($siteName ?? ''), ENT_QUOTES, 'UTF-8') ?></title>
     <?php endif; ?>
     <?= $this->callMacro('wpHead', []) ?>
     
 </head>
-<body class="<?= htmlspecialchars((string)($bodyClasses ?? ''), ENT_QUOTES, 'UTF-8') ?>">
+<body class="<?= htmlspecialchars((string)($bodyClasses ?? ''), ENT_QUOTES, 'UTF-8') ?> <?= $this->callMacro('extraBodyClass', []) ?>">
     <a class="skip-link" href="#main">Aller au contenu</a>
     
-        [% include 'partials/header.html.tpl' %]
+        <?= $this->renderInclude('partials/header.html.tpl', array_merge(get_defined_vars(), [])) ?>
     
     
     <main id="main" class="site-main">
         
-    [% include 'partials/breadcrumbs.html.tpl' %]
+    <?= $this->renderInclude('partials/breadcrumbs.html.tpl', array_merge(get_defined_vars(), [])) ?>
     <article class="page page--<?= htmlspecialchars((string)(\Lunar\Template\Runtime\Access::get($post, 'slug') ?? ''), ENT_QUOTES, 'UTF-8') ?>" lang="<?= htmlspecialchars((string)(\Lunar\Template\Runtime\Access::get(\Lunar\Template\Runtime\Access::get($post, 'language'), 'code') ?? ''), ENT_QUOTES, 'UTF-8') ?>">
-        <header class="page__header">
-            <h1 class="page__title"><?= htmlspecialchars((string)(\Lunar\Template\Runtime\Access::get($post, 'title') ?? ''), ENT_QUOTES, 'UTF-8') ?></h1>
-        </header>
-        <?php if (!empty(\Lunar\Template\Runtime\Access::get($post, 'featuredImageUrl'))): ?>
+        <?php if (!empty($coverHtml)): ?>
+            <div class="page__cover"><?= $coverHtml ?? '' ?></div>
+        <?php elseif (!empty(\Lunar\Template\Runtime\Access::get($post, 'featuredImageUrl'))): ?>
             <figure class="page__featured">
                 <img
                     class="page__featured-image"
@@ -35,14 +34,17 @@
                     loading="lazy">
             </figure>
         <?php endif; ?>
+        <header class="page__header">
+            <h1 class="page__title"><?= htmlspecialchars((string)(\Lunar\Template\Runtime\Access::get($post, 'title') ?? ''), ENT_QUOTES, 'UTF-8') ?></h1>
+        </header>
         <div class="page__content">
-            <?= \Lunar\Template\Runtime\Access::get($post, 'content') ?? '' ?>
+            <?= $bodyHtml ?? '' ?>
         </div>
     </article>
 
     </main>
     
-    [% include 'partials/footer.html.tpl' %]
+    <?= $this->renderInclude('partials/footer.html.tpl', array_merge(get_defined_vars(), [])) ?>
     <?= $this->callMacro('wpFooter', []) ?>
     
 </body>
